069349b20fc1531066f531cf8d8d4429b0d4730f
Mauro Farina <maurofarina@protonmail.com>
2022-12-23 18:22:10 +0100

    minor refactor

--- app/app.js
+++ app/app.js
@@ -16,41 +16,6 @@ app.use(express.json());
 app.use(express.static(__dirname +"/public"));
 
 app.listen(process.env.PORT, async () => {
-    console.log("listening on port 8080");
+    console.log(`listening on port {process.env.PORT}`);
     await mongoManager.connect().then(console.log("Connected to MongoDB"));
-    console.log("hey hey! server is up and runnning");
-    // either i remove volume (it works but nodemon doesnt)
-    // or i add volume (nodemon works, but i need to keep and copy node_modules as well...)
-    // https://stackoverflow.com/a/35317425
 });
-/*
-app.get("/test-find", async (req, res) => {
-    const mongo = mongoManager.getDB();
-    let dbResults = await mongo.collection("socialnetwork").find().toArray();
-    console.log(dbResults);
-    res.send("done! check console log");
-});
-
-app.get("/test-add-1", async (req, res) => {
-    console.log("test-add-1");
-    const mongo = mongoManager.getDB();
-    const document = {
-        name: "John",
-        age: 18,
-        hobbies: ["books", "clothes"]
-    };
-    console.log("about to add document");
-    await mongo.collection("socialnetwork").insertOne(document, async (err, result) => {
-        if (err) {
-            console.log("oh noooo!");
-            console.log(err);
-            client.close();
-            return;
-        } else {
-            console.log("no problemo! document added with no problems");
-            console.log(await mongo.collection("socialnetwork").find().toArray());
-        }
-    });
-    res.send("done!");
-});
-*/
