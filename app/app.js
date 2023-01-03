319c0142056401b6e8d7b8a68dd58ba1b0fc6f9d
Mauro Farina <maurofarina@protonmail.com>
2023-01-03 14:48:47 +0100

    utils api moved to another file

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const authAPIRouter = require("./auth-router.js");
 const socialAPIRouter = require("./social-router.js");
+const utilsAPIRouter = require("./private/utils.js");
 const mongoManager = require("./mongodb-manager.js");
 const cookieParser = require('cookie-parser');
 const { config } = require("dotenv");
@@ -12,29 +13,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authAPIRouter);
 app.use("/api/social", socialAPIRouter);
+app.use("/api/utils", utilsAPIRouter);
 
 app.use(express.static(__dirname +"/public/"));;
 
 app.listen(process.env.PORT, async () => {
     console.log(`listening on port ${process.env.PORT}`);
     await mongoManager.connect().then(console.log("Connected to MongoDB"));
-});
-
-app.get("/clear/:collectionName", async (req, res) => {
-    const mongo = mongoManager.getDB();
-    let result;
-    try{
-        result = await mongo.collection(req.params.collectionName).deleteMany({});
-    } catch(error) {
-        res.send(`ERROR while deleting documents in collection ${req.params.collectionName}`);
-    }
-    res.json({
-        deleted : result.deletedCount
-    })
-});
-
-app.get("/collection/:name", async (req, res) => {
-    const mongo = mongoManager.getDB();
-    let allUsersQuery = await mongo.collection(req.params.name).find({}).toArray();
-    res.json(allUsersQuery);
 });
