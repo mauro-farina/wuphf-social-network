219e3e17ffe2692ebf7e64572f6ea63a336670b5
Mauro Farina <maurofarina@protonmail.com>
2023-01-12 13:32:17 +0100

    undo: returns username who toggled like

--- app/routers/social.js
+++ app/routers/social.js
@@ -265,16 +265,16 @@ router.get("/feed", validateAuthCookie, async (req, res) => { // List of message
 router.post("/like/:messageID", validateAuthCookie, sanitizeParamMessageID, async (req, res) => { // req.username likes message `:messageID`
     const cookieUsername = req.username;
     const mongo = mongoManager.getDB();
-    let updateLikes = await mongo.collection("messages").updateOne( {messageID : parseInt(req.params.messageID)}, {$push: {likedBy: cookieUsername}} );
-    res.json({"messageID" : req.params.messageID, "likes" : updateLikes.likedBy.length});
+    await mongo.collection("messages").updateOne( {messageID : parseInt(req.params.messageID)}, {$push: {likedBy: cookieUsername}} );
+    res.json({"messageID" : req.params.messageID, "likedToggledBy" : cookieUsername});
 });
 
 
 router.delete("/like/:messageID", validateAuthCookie, sanitizeParamMessageID, async (req, res) => { // req.username remove like to message `:messageID`
     const cookieUsername = req.username;
     const mongo = mongoManager.getDB();
-    let updateLikes = await mongo.collection("messages").updateOne( {messageID : parseInt(req.params.messageID)}, {$pull: {likedBy: cookieUsername}} );
-    res.json({"messageID" : req.params.messageID, "likes" : updateLikes.likedBy.length});
+    await mongo.collection("messages").updateOne( {messageID : parseInt(req.params.messageID)}, {$pull: {likedBy: cookieUsername}} );
+    res.json({"messageID" : req.params.messageID, "likedToggledBy" : cookieUsername});
 });
 
 
